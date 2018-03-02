c0d4ffbf683a0d0340b9fbbb0a66dd63ed6b2598
Fabio Garcia <fagarbal@gmail.com>
2018-03-02 15:30:30 +0100

    Added Card Types - Diamond class, include Diamond card type test

--- spec/CardTypesSpec.js
+++ spec/CardTypesSpec.js
@@ -1,11 +1,20 @@
 describe("Card Types", () => {
   var spade;
+  var diamond;
 
   beforeEach(() => {
     spade = new Spade();
+    diamond = new Diamond();
   });
 
   it("should be a Spade", () => {
     expect(spade.constructor).toBe(Spade);
   });
+
+  it("should be a Diamond", () => {
+    expect(diamond.constructor).toBe(Diamond);
+  });
+
+  
+
 });
--- src/CardTypes.js
+++ src/CardTypes.js
@@ -1,1 +1,3 @@
 class Spade{}
+
+class Diamond{}
